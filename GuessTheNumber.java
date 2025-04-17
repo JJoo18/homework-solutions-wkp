91c189fd651d5cd94880a6cdc30191dbd036852d
JJoo18 <fijobuto@gmail.com>
2025-04-17 10:43:14 +0200

    1st Exercise

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
-        int NumberToGuess = (int) (Math.random()*20) + 1;
+        Scanner diff = new Scanner(System.in);
+        System.out.println("Choose your how many Trys you want:");
+        int tries = diff.nextInt();
+        System.out.println("Choose the range of numbers:");
+        int range = diff.nextInt();
+
+        int NumberToGuess = (int) (Math.random()*range) + 1;
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 5; i++) {
+        System.out.println("It's time to guess:");
+        for (int i = 0; i < tries; i++) {
             int s = scanner.nextInt();
-            int g = 4 - i;
+            int g = tries - 1 - i;
             if (s == NumberToGuess) {
                 System.out.println("Correct!");
                 break;
             }else {
-                System.out.println("You were wrong!");
                 if (s > NumberToGuess) {
-                    System.out.println("Too High!");
+                    System.out.println("Wrong, too High!");
                 }
                 if (s < NumberToGuess){
-                    System.out.println("Too Low!");
+                    System.out.println("Wrong, too Low!");
                 }
                 System.out.println("Guesses remaining: " + g);
+                if (i == tries - 1) {
+                    System.out.println("You exceed the range of Tries. Better luck next time!");
+                }
             }
         }
     }
